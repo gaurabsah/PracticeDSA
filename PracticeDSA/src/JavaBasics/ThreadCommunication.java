400e5afa1b773cf7818e07841b586ac5d531ed42
Gaurab Sah <gaurab.sah.777@gmail.com>
2025-03-21 20:55:47 +0530

    thread

--- PracticeDSA/src/JavaBasics/ThreadCommunication.java
+++ PracticeDSA/src/JavaBasics/ThreadCommunication.java
@@ -1,9 +1,72 @@
 package JavaBasics;
-class Chat{
+class SharedResource {
+    private int data;
+    private boolean available = false;
 
+    public synchronized void put(int value) {
+        while (available) {
+            try {
+                wait(); // Wait if data is already available
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        data = value;
+        available = true;
+        System.out.println("Produced: " + data);
+        notify(); // Notify the consumer
+    }
+
+    public synchronized int get() {
+        while (!available) {
+            try {
+                wait(); // Wait if no data is available
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        available = false;
+        System.out.println("Consumed: " + data);
+        notify(); // Notify the producer
+        return data;
+    }
 }
+
+class Producer extends Thread {
+    private SharedResource resource;
+
+    public Producer(SharedResource resource) {
+        this.resource = resource;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            resource.put(i);
+        }
+    }
+}
+
+class Consumer extends Thread {
+    private SharedResource resource;
+
+    public Consumer(SharedResource resource) {
+        this.resource = resource;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            resource.get();
+        }
+    }
+}
+
 public class ThreadCommunication {
     public static void main(String[] args) {
+        SharedResource resource = new SharedResource();
+        Producer producer = new Producer(resource);
+        Consumer consumer = new Consumer(resource);
 
+        producer.start();
+        consumer.start();
     }
 }
--- PracticeDSA/src/JavaBasics/VolatileKeywordDemo.java
+++ PracticeDSA/src/JavaBasics/VolatileKeywordDemo.java
@@ -1,4 +1,26 @@
 package JavaBasics;
 
+class SharedResources {
+    private volatile boolean running = true; // Ensures visibility
+
+    public void stop() {
+        running = false; // Immediately visible to all threads
+    }
+
+    public void run() {
+        while (running) { }
+        System.out.println("Stopped!");
+    }
+}
+
 public class VolatileKeywordDemo {
+    public static void main(String[] args) throws InterruptedException {
+        SharedResources resource = new SharedResources();
+
+        Thread t1 = new Thread(resource::run);
+        t1.start();
+
+        Thread.sleep(1000);
+        resource.stop(); // Now, t1 will see this change and exit
+    }
 }
